8711d2ec2971bc7e09e8f881b9054708beb0ef10
Denisssska <yarmoshkoden18m@gmail.com>
2022-10-28 06:48:50 +0300

    update

--- src/header/sectionForProject/SectionForProject.tsx
+++ src/header/sectionForProject/SectionForProject.tsx
@@ -15,27 +15,31 @@ export const SectionForProject = () => {
                         <div className={c.left}>
                             <div className={c.box}>
                                 <div className={c.number}>01</div>
-                                <a target={"_blank"} href={'https://todolist000incubator.herokuapp.com/'}>View
-                                    project</a>
-                                <div className={c.textInBox}>
 
+                                <div className={c.textInBox}>
                                     <h1>todolist</h1>
+                                    <a target={"_blank"} href={'https://todolist000incubator.herokuapp.com/'}>View
+                                        project</a>
                                 </div>
                             </div>
                             <div className={c.box}>
                                 <div className={c.number}>02</div>
-                                <a target={"_blank"} href={'https://front-posts-denisssska.vercel.app/'}>View
-                                    project</a>
-                                <div className={c.textInBox}><h1>Blog</h1></div>
+
+                                <div className={c.textInBox}><h1>Blog</h1>
+                                    <a target={"_blank"} href={'https://front-posts-denisssska.vercel.app/'}>View
+                                        project</a>
+                                </div>
                             </div>
                         </div>
                         <div className={c.right}>
 
                             <div className={c.box}>
                                 <div className={c.number}>03</div>
-                                <a target={"_blank"} href={'https://Denisssska.github.io/friday_project'}>View
-                                    project</a>
-                                <div className={c.textInBox}><h1>Cards</h1></div>
+                                <div className={c.textInBox}>
+                                    <h1>Cards</h1>
+                                    <a target={"_blank"} href={'https://Denisssska.github.io/friday_project'}>View
+                                        project</a>
+                                </div>
                             </div>
                             <div className={c.box}>
                                 <div className={c.number}>04</div>
